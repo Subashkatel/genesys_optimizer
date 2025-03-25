cad1f73e7ac06a09b0bd5f39c9fa35b64582f3ef
SubashKatel <skatel@ucsd.com>
2025-03-25 00:25:25 -0700

    fix: logging

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -1,30 +1,40 @@
 import logging
 import sys
 
-def setup_logging(name="genesys_optimizer", level=logging.INFO, log_file=None):
-    """Setting up a logger with proper formating
-
-    Args:
-        name (str, optional): _description_. Defaults to "genesys_optimizer".
-        level (_type_, optional): _description_. Defaults to logging.INFO.
-        log_file (_type_, optional): _description_. Defaults to None.
+def setup_logging(name, level=logging.INFO, log_file=None):
     """
+    Set up and configure a logger
     
+    Args:
+        name (str): Logger name
+        level (int): Logging level
+        log_file (str, optional): Path to log file
+        
+    Returns:
+        logging.Logger: Configured logger instance
+    """
+    # Create logger
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    # setup console handler - ch
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(level)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-    # setup file handler - fh
-    if log_file:
-        fh = logging.FileHandler(log_file)
-        fh.setLevel(level)
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
+    
+    # Avoid adding handlers if they already exist
+    if not logger.handlers:
+        # Console handler
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(level)
+        
+        # Formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        console_handler.setFormatter(formatter)
+        
+        # Add console handler to logger
+        logger.addHandler(console_handler)
+        
+        # File handler if specified
+        if log_file:
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setLevel(level)
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
     
     return logger
